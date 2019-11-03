6603581c821fbb35b5d1a5d6b3a54bf0a15647cf
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2019-11-04 10:29:12 +1100

    feat: speed, logging, validation, defaults, docs

--- README.md
+++ README.md
@@ -1,91 +1,116 @@
 # create-ut
 
-Tool for creating UT microservices and applications
+Tool for creating `applications`, `microservices`, `ports`, etc.,
+based on UnderTree framework.
 
 ## Usage
 
-* use `npm init`
+Use with `npm init`
 
-    User creates app, microservice, port
-    or anything else by running
+User creates `application`, `microservice`, `port` or anything else by running:
 
-    ```bash
-    # create application
-    npm init ut
-    npm init ut app[-kind]
+* create `application` in the current folder named `impl-*`
 
-    # create microservice
-    npm init ut ms[-kind]
-    npm init ut service[-kind]
-    npm init ut microservice[-kind]
+  ```bash
+  npm init ut
+  npm init ut app[-kind]
+  ```
 
-    # create port
-    npm init ut port[-kind]
-    ```
+* create `application` in a new folder named `impl-name`
+
+  ```bash
+  npm init ut app[-kind] impl-name
+  ```
+
+* create `microservice` in the current folder named `ut-*`
+
+  ```bash
+  npm init ut ms[-kind]
+  npm init ut service[-kind]
+  npm init ut microservice[-kind]
+  ```
+
+* create `microservice` in new folder named `ut-name`
+
+  ```bash
+  npm init ut ms[-kind] ut-name
+  npm init ut service[-kind] ut-name
+  npm init ut microservice[-kind] ut-name
+  ```
+
+* create `port` in the current folder named `ut-port-*`
+
+  ```bash
+  npm init ut port[-kind]
+  ```
+
+* create `port` in new folder named `ut-port-name`
+
+  ```bash
+  npm init ut port[-kind] ut-port-name
+  ```
 
 ## Algorithm
 
-1) Clone latest version of package `ut-app[-kind]` or `ut-microservice[-kind]`.
+1) Clone latest version of package `impl-application[-kind]` or `ut-microservice[-kind]`.
    Obtain repository by using `package-json` in a way similar to:
 
    ```js
    const gitUrl = async () =>
-      await require('package-json')('ut-app', {registryUrl: 'nexus.softwaregroup.com'})).repository.url
+      await require('package-json')('impl-application', {registryUrl: 'nexus.softwaregroup.com'})).repository.url
    }
    ```
 
-1) Require the modules `.ut-create` in the cloned folder, having the following
-   structure:
+1) Require the module `.ut-create` in the cloned folder, having the following structure:
 
-```js
-module.exports = {
-    params: {
-        schema: {
-            properties: {
-                id: {
-                    type: 'string',
-                    title: 'Package identifier'
-                },
-                title: {
-                    type: 'string',
-                    title: 'Package title'
-                },
-                userName: {
-                    type: 'string',
-                    title: 'User name',
-                    default: 'based on output from git config --get user.email'
-                }
-            },
-            required: ['id', 'title', 'userName']
-        },
-        uiSchema: {
-
-        }
-    },
-    rename: ({id, title, userName}) => [{
-        files: 'server/**/*.js',
-        replace: [
-            /implementation: 'product'/,
-            `implementation: '${id}'`
-        ]
-    }, {
-        files: 'server/**/*.json',
-        replace: [
-            /"implementation": "product"/,
-            `"implementation": '${id}'`
-        ]
-    }, {
-        files: 'ut_*_rc', // example is for YAML based rc
-        replace: [[
-            /database: impl-firstName-lastName/,
-            `database: impl-${userName.split('.').join('-')}`
-        ],[
-            /user: firstName\.lastName/,
-            `user: ${userName}`
-        ]]
-    } /*etc*/]
-};
-```
+   ```js
+   module.exports = {
+       params: {
+           schema: {
+               properties: {
+                   id: {
+                       type: 'string',
+                       title: 'Package identifier'
+                   },
+                   title: {
+                       type: 'string',
+                       title: 'Package title'
+                   },
+                   userName: {
+                       type: 'string',
+                       title: 'User name',
+                       default: 'based on output from git config --get user.email'
+                   }
+               },
+               required: ['id', 'title', 'userName']
+           },
+           uiSchema: {
+           }
+       },
+       rename: ({id, title, userName}) => [{
+           files: 'server/**/*.js',
+           replace: [
+               /implementation: 'product'/,
+               `implementation: '${id}'`
+           ]
+       }, {
+           files: 'server/**/*.json',
+           replace: [
+               /"implementation": "product"/,
+               `"implementation": '${id}'`
+           ]
+       }, {
+           files: 'ut_*_rc', // example is for YAML based rc
+           replace: [[
+               /database: impl-firstName-lastName/,
+               `database: impl-${userName.split('.').join('-')}`
+           ],[
+               /user: firstName\.lastName/,
+               `user: ${userName}`
+           ]]
+       } /*etc*/]
+   };
+   ```
 
 1) Use `ut-form-jsonschema` to create a form based on the `params` property
    in `create.js`.
--- index.js
+++ index.js
@@ -10,28 +10,28 @@ const path = require('path');
 const fs = require('fs');
 const { edit } = require('ut-form-jsonschema');
 const glob = require('glob');
-const UtLog = require('ut-log');
-const log = new UtLog({
-    type: 'bunyan',
-    name: 'create-ut',
-    service: 'create-ut',
-    version,
-    streams: []
-}).createLog('info', {name: 'create-ut', context: 'create-ut'});
+const log = {
+    fatal: function() { console.error(...arguments); },
+    error: function() { console.error(...arguments); },
+    warn: function() { console.log(...arguments); },
+    info: function() { console.log(...arguments); },
+    debug: function() { console.log(...arguments); },
+    trace: function() { console.log(...arguments); }
+};
 
 function exec(cmd, args, options) {
     const res = childProcess.spawnSync(cmd, args, options);
 
-    if (res.stderr) console.error(res.stderr.toString().trim());
+    if (res.stderr) log.error(res.stderr.toString().trim());
 
     if (res.error) {
-        console.error(cmd, args.join(' '), '=>');
-        console.error(res.error);
+        log.error(cmd, args.join(' '), '=>');
+        log.error(res.error);
         return process.exit(1);
     }
 
     if (res.status !== 0) {
-        console.error(cmd, args.join(' '), '=>', res.status);
+        log.error(cmd, args.join(' '), '=>', res.status);
         return process.exit(1);
     }
 
@@ -48,7 +48,7 @@ const formDataDefaults = [
 ];
 
 async function run() {
-    let template, dir, root;
+    let template, root, title;
 
     try {
         const program = new commander.Command(name)
@@ -57,37 +57,49 @@ async function run() {
             .arguments('[template] [project-directory] [options...]')
             .allowUnknownOption()
             .usage('[template] [project-directory] [options...]')
-            .action((kind = 'app', prjDir = '.') => {
+            .action((kind = 'app', dir = '.') => {
                 const parts = kind.split('-');
+                root = path.resolve(dir);
+                const prefix = path.basename(root).split('-')[0];
+                const suffix = path.basename(root).replace(/^(impl-|ut-)/, '');
+                if (!['impl', 'ut'].includes(prefix)) throw new Error('Project directory should be prefixed with "impl-" or "ut-"');
                 switch (parts[0]) {
                     case 'ms':
                     case 'service':
                     case 'microservice':
+                        title = 'Microservice ' + suffix;
                         parts[0] = 'ut-microservice';
                         break;
                     case 'port':
+                        title = 'Port ' + suffix;
                         parts[0] = 'ut-port-template';
                         break;
                     case 'app':
+                        title = 'Implementation ' + suffix;
                         parts[0] = 'impl-application';
                         break;
                     default:
-                        parts.unshift('impl');
+                        title = 'Module ' + suffix;
+                        parts.unshift(prefix);
                         break;
                 }
                 template = parts.join('-');
-                dir = prjDir;
-                root = path.join(process.cwd(), dir);
             })
             .parse(process.argv);
 
-        const { repository: { url } } = await packageJson(template, {registryUrl: 'https://nexus.softwaregroup.com/repository/npm-all/'});
+        const {repository: {url}, description} = await packageJson(template, {registryUrl: 'https://nexus.softwaregroup.com/repository/npm-all/'});
 
-        exec('git', ['clone', url, dir], {stdio: 'inherit'});
+        log.debug(`Creating "${description}" in ${root}`);
+        exec('git', ['clone', url, root], {stdio: 'inherit'});
 
         const {params, rename} = require(path.join(root, '.ut-create'));
 
-        params.formData = {...minimist(remainingArgs(program)), ...params.formData};
+        params.formData = {
+            id: path.basename(root),
+            title,
+            ...minimist(remainingArgs(program)),
+            ...params.formData
+        };
 
         formDataDefaults.forEach(({alias, value}) => {
             const key = alias.find(key => params.schema.properties[key]);
@@ -96,16 +108,36 @@ async function run() {
 
         const {url: { href }, id} = await edit({log});
 
+        log.debug('Opening configuration URL');
         childProcess.exec((process.platform === 'win32' ? 'start' : 'xdg-open') + ' ' + href, err => {
             if (err) {
-                console.error(err);
-                console.log('Open configuration form at:', href);
+                log.error(err);
+                log.debug('Open manually configuration form at:', href);
             }
         });
 
-        const data = await edit({ ...params, id, log });
+        const data = await edit({...params,
+            id,
+            log,
+            submit: async({payload}) => {
+                return {
+                    payload: {
+                        state: {
+                            schema: {
+                                title: 'Configuration completed',
+                                description: 'You can now close this page',
+                                type: 'object',
+                                properties: {}
+                            },
+                            buttons: []
+                        }
+                    }
+                };
+            }
+        });
         const rules = rename(data);
 
+        log.debug('Applying parameters');
         rules.forEach(({files, replace}) => {
             const list = glob.sync(/^[^/\\].*/.test(files) ? '/' + files : files, { root });
             list.forEach(file => {
@@ -121,11 +153,12 @@ async function run() {
             });
         });
 
+        log.debug('Setting new remote URL');
         exec('git', ['remote', 'set-url', 'origin', url.replace(template, path.basename(root))], {stdio: 'inherit', cwd: root});
 
-        console.log(`${template} based project has been successfully created in folder ${root}`);
+        log.debug(`${description} based project has been successfully created in folder ${root}`);
     } catch (e) {
-        console.error(e);
+        log.error(e);
         process.exit(1);
     }
 }
